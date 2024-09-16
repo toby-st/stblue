527ed933d942b6275cba312b4b19ea75b13813a5
Toby S <toby-st@refo.pw>
2024-09-17 00:24:01 +0200

    feat: add Threema

--- versions/laptop/extras.sh
+++ versions/laptop/extras.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rpm-ostree install $(curl -s https://api.github.com/repos/VSCodium/vscodium/releases/latest | grep "browser_download_url.*x86_64.rpm\"" | cut -d : -f 2,3 | tr -d \")
+rpm-ostree install $(curl -s https://api.github.com/repos/VSCodium/vscodium/releases/latest | grep "browser_download_url.*x86_64.rpm\"" | cut -d : -f 2,3 | tr -d \") https://releases.threema.ch/web-electron/v1/release/Threema-Latest.rpm
