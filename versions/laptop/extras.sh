c6e9eadad22703025757fad4659fcf32873c3cd7
Toby S <toby-st@refo.pw>
2024-10-13 14:11:16 +0200

    Update extras.sh

--- versions/laptop/extras.sh
+++ versions/laptop/extras.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rpm-ostree install $(curl -s https://api.github.com/repos/VSCodium/vscodium/releases/latest | grep "browser_download_url.*x86_64.rpm\"" | cut -d : -f 2,3 | tr -d \") \
-https://releases.threema.ch/web-electron/v1/release/Threema-Latest.rpm \
-https://repo.protonvpn.com/fedora-40-stable/proton-vpn-gnome-desktop/proton-vpn-gnome-desktop-0.8.0-1.fc40.noarch.rpm
+https://releases.threema.ch/web-electron/v1/release/Threema-Latest.rpm
+rpm-ostree install --releasever 40 proton-vpn-gnome-desktop
