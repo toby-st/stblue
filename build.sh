9fa04c423eaecc9de3b7879970dd5eb6b94943e0
Toby S <toby-st@refo.pw>
2024-11-06 17:21:46 +0100

    fix: temporary ProtonVPN fix (#22)

--- build.sh
+++ build.sh
@@ -16,9 +16,7 @@ rpm-ostree install \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm \
     https://repo.protonvpn.com/fedora-${RELEASE}-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.1-2.noarch.rpm
 
-if [[ "$RELEASE" == "39" ]]; then
-    sed -i 's%free/fedora/releases%free/fedora/development%' /etc/yum.repos.d/rpmfusion-*.repo
-fi
+sed -i 's/gpgcheck = 1/gpgcheck = 0/g' /etc/yum.repos.d/protonvpn-stable.repo
 
 rpm-ostree override remove \
     ${REMOVE_PACKAGES[@]} \
