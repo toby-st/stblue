f72c5773085744d872756de107958b2bdc884601
Toby S <toby-st@refo.pw>
2024-11-26 01:37:31 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,11 +10,14 @@ REMOVE_PACKAGES=($(jq -r "(.remove) | sort | unique[]" /tmp/packages.json))
 REMOVE_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${REMOVE_PACKAGES[@]}))
 
 curl -L https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo
+curl -L https://repo.protonvpn.com/fedora-41-stable/public_key.asc -o /tmp/proton-key.asc
 
 rpm-ostree install \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm \
     https://repo.protonvpn.com/fedora-${RELEASE}-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.2-1.noarch.rpm
+
+    sed sed -i 's|gpgkey = https://repo.protonvpn.com/fedora-\$releasever-stable/public_key.asc|gpgkey = /tmp/proton-key.asc|g' /etc/yum.repos.d/protonvpn-stable.repo
     
 rpm-ostree override remove \
     ${REMOVE_PACKAGES[@]} \
