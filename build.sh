cd8e93cf50edacadeea3329c6b5756de59ff1bba
Toby S <toby-st@refo.pw>
2024-11-26 01:47:04 +0100

    chore: update ProtonVPN (#25)

--- build.sh
+++ build.sh
@@ -10,14 +10,15 @@ REMOVE_PACKAGES=($(jq -r "(.remove) | sort | unique[]" /tmp/packages.json))
 REMOVE_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${REMOVE_PACKAGES[@]}))
 
 curl -L https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo
+curl -L https://repo.protonvpn.com/fedora-41-stable/public_key.asc -o /tmp/proton-key.asc
 
 rpm-ostree install \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm \
-    https://repo.protonvpn.com/fedora-${RELEASE}-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.1-2.noarch.rpm
+    https://repo.protonvpn.com/fedora-${RELEASE}-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.2-1.noarch.rpm
 
 sed -i 's/gpgcheck = 1/gpgcheck = 0/g' /etc/yum.repos.d/protonvpn-stable.repo
-
+    
 rpm-ostree override remove \
     ${REMOVE_PACKAGES[@]} \
     $(printf -- "--install=%s " ${INSTALL_PACKAGES[@]})
