55931aaa7202e82aabf4521073a729f152d1a219
Toby S <toby-st@refo.pw>
2024-11-26 01:24:15 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,10 +14,8 @@ curl -L https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repo
 rpm-ostree install \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm \
-    https://repo.protonvpn.com/fedora-${RELEASE}-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.1-2.noarch.rpm
-
-sed -i 's/gpgcheck = 1/gpgcheck = 0/g' /etc/yum.repos.d/protonvpn-stable.repo
-
+    https://repo.protonvpn.com/fedora-${RELEASE}-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.2-1.noarch.rpm
+    
 rpm-ostree override remove \
     ${REMOVE_PACKAGES[@]} \
     $(printf -- "--install=%s " ${INSTALL_PACKAGES[@]})
