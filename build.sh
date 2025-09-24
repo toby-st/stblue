2afd3198185ec23950499c2f2f1c2cf0000661bb
toby-st <toby-st@refo.pw>
2025-09-24 19:16:08 +0200

    fix: temp enable testing repos to fix dependency issues

--- build.sh
+++ build.sh
@@ -9,10 +9,12 @@ REMOVE_PACKAGES=($(jq -r "(.remove) | sort | unique[]" /tmp/packages.json))
 
 REMOVE_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${REMOVE_PACKAGES[@]}))
 
+
 rpm-ostree install \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm \
     https://repo.protonvpn.com/fedora-${RELEASE}-unstable/protonvpn-beta-release/protonvpn-beta-release-1.0.3-1.noarch.rpm
+sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-free-updates-testing.repo && sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree-updates-testing.repo
 
 sed -i 's/gpgcheck = 1/gpgcheck = 0/g' /etc/yum.repos.d/protonvpn-beta.repo
     
