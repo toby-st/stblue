cde077a70afb0ca0166c1daa5e2d2fdaf2116cd5
Toby S <toby-st@refo.pw>
2025-10-04 15:02:39 +0200

    feat: swtich to vscode (#31)

--- build.sh
+++ build.sh
@@ -9,7 +9,9 @@ REMOVE_PACKAGES=($(jq -r "(.remove) | sort | unique[]" /tmp/packages.json))
 
 REMOVE_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${REMOVE_PACKAGES[@]}))
 
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
 rpm-ostree install \
     https://repo.protonvpn.com/fedora-42-unstable/protonvpn-beta-release/protonvpn-beta-release-1.0.3-1.noarch.rpm
 
